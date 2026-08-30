6004d9fb5aeb804b5e4df9c10d45bb7ca79bdd31
Samar Talwar <samartalwarzillion001@gmail.com>
2026-08-31 02:03:12 +0530

    Remove temporary files

--- rewrite.py
+++
@@ -1,7 +0,0 @@
-if commit.author_email == b"bhalabalaram@gmail.com":
-    commit.author_email = b"samartalwarzillion001@gmail.com"
-    commit.author_name = b"Samar Talwar"
-
-if commit.committer_email == b"bhalabalaram@gmail.com":
-    commit.committer_email = b"samartalwarzillion001@gmail.com"
-    commit.committer_name = b"Samar Talwar"
