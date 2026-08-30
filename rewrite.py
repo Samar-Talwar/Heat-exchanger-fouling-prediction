8c386637d71ba0fe4550cf56c75ee33c6cb8e1f5
Samar Talwar <samartalwarzillion001@gmail.com>
2026-08-31 02:02:12 +0530

    Initial commit

---
+++ rewrite.py
@@ -0,0 +1,7 @@
+if commit.author_email == b"bhalabalaram@gmail.com":
+    commit.author_email = b"samartalwarzillion001@gmail.com"
+    commit.author_name = b"Samar Talwar"
+
+if commit.committer_email == b"bhalabalaram@gmail.com":
+    commit.committer_email = b"samartalwarzillion001@gmail.com"
+    commit.committer_name = b"Samar Talwar"
